d50ec25bbf77af6b0636436afbcc9478d453f232
valin000 <765167664@qq.com>
2018-07-12 15:52:08 +0800

    Add files via upload

--- basic/bin-debug/Main.js
+++ basic/bin-debug/Main.js
@@ -1,3 +1,4 @@
+/**基本显示 */
 var __reflect = (this && this.__reflect) || function (p, c, t) {
     p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
 };
@@ -12,18 +13,16 @@ var Main = (function (_super) {
     __extends(Main, _super);
     function Main() {
         var _this = _super.call(this) || this;
-        _this.startLoad();
+        _this.once(egret.Event.ADDED_TO_STAGE, _this.onAddToStage, _this);
         return _this;
     }
-    Main.prototype.startLoad = function () {
-        var loader = new egret.ImageLoader();
-        loader.addEventListener(egret.Event.COMPLETE, this.onLoadComplete, this);
-        var url = "resource/assets/cartoon-egret_00.png";
-        loader.load(url);
+    Main.prototype.onAddToStage = function (event) {
+        var imgLoader = new egret.ImageLoader();
+        imgLoader.once(egret.Event.COMPLETE, this.imgLoadHandler, this);
+        imgLoader.load("resource/assets/cartoon-egret_00.png");
     };
-    Main.prototype.onLoadComplete = function (event) {
-        var loader = event.target;
-        var bmd = loader.data;
+    Main.prototype.imgLoadHandler = function (evt) {
+        var bmd = evt.currentTarget.data;
         var texture = new egret.Texture();
         texture.bitmapData = bmd;
         var bird = new egret.Bitmap(texture);
@@ -44,7 +43,8 @@ var Main = (function (_super) {
         this._txInfo.type = egret.TextFieldType.DYNAMIC;
         this._txInfo.lineSpacing = 6;
         this._txInfo.multiline = true;
-        this._txInfo.text = "轻触屏幕调整显示对象位置";
+        this._txInfo.text =
+            "轻触屏幕调整显示对象位置";
         this._bgInfo = new egret.Shape;
         this.addChildAt(this._bgInfo, this.numChildren - 1);
         this._bgInfo.x = this._txInfo.x;
--- basic/src/Main.ts
+++ basic/src/Main.ts
@@ -1,37 +1,37 @@
+/**基本显示 */
+
 class Main extends egret.DisplayObjectContainer {
     public constructor() {
         super();
 
-        this.startLoad();
+        this.once(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
     }
 
-    private startLoad():void {
-        var loader:egret.ImageLoader = new egret.ImageLoader();
-        loader.addEventListener(egret.Event.COMPLETE, this.onLoadComplete, this);
-        var url:string = "resource/assets/cartoon-egret_00.png";
-        loader.load(url);
+    private onAddToStage(event:egret.Event): void {
+        var imgLoader: egret.ImageLoader = new egret.ImageLoader();
+        imgLoader.once( egret.Event.COMPLETE, this.imgLoadHandler, this);
+        imgLoader.load("resource/assets/cartoon-egret_00.png");   
     }
 
     private _txInfo:egret.TextField;
     private _bgInfo:egret.Shape;
 
-    private onLoadComplete(event:egret.Event):void {
-        var loader:egret.ImageLoader = <egret.ImageLoader>event.target;
-        var bmd:egret.BitmapData = loader.data;
+    private imgLoadHandler(evt: egret.Event): void {
+        var bmd: egret.BitmapData = evt.currentTarget.data;
         var texture = new egret.Texture();
         texture.bitmapData = bmd;
         var bird: egret.Bitmap = new egret.Bitmap(texture);
         bird.x = 100;
         bird.y = 100;
-        this.addChild(bird);
+        this.addChild( bird );
 
         bird.anchorOffsetX = bmd.width / 2;
         bird.anchorOffsetY = bmd.height / 2;
         bird.x = this.stage.stageWidth * .5;
-        bird.y = this.stage.stageHeight* .5;
+        bird.y = this.stage.stageHeight * .5;
 
         this._txInfo = new egret.TextField;
-        this.addChild(this._txInfo);
+        this.addChild( this._txInfo );
 
         this._txInfo.size = 28;
         this._txInfo.x = 50;
@@ -41,21 +41,23 @@ class Main extends egret.DisplayObjectContainer {
         this._txInfo.type = egret.TextFieldType.DYNAMIC;
         this._txInfo.lineSpacing = 6;
         this._txInfo.multiline = true;
-        this._txInfo.text = "轻触屏幕调整显示对象位置";
+        this._txInfo.text =
+                "轻触屏幕调整显示对象位置";
 
         this._bgInfo = new egret.Shape;
-        this.addChildAt(this._bgInfo, this.numChildren - 1);
-        
+        this.addChildAt( this._bgInfo, this.numChildren - 1 );
+
         this._bgInfo.x = this._txInfo.x;
         this._bgInfo.y = this._txInfo.y;
         this._bgInfo.graphics.clear();
-        this._bgInfo.graphics.beginFill(0xffffff, .5);
-        this._bgInfo.graphics.drawRect(0, 0, this._txInfo.width, this._txInfo.height);
+        this._bgInfo.graphics.beginFill( 0xffffff, .5 );
+        this._bgInfo.graphics.drawRect( 0, 0, this._txInfo.width, this._txInfo.height );
         this._bgInfo.graphics.endFill();
-
-        this.stage.addEventListener(egret.TouchEvent.TOUCH_BEGIN, (evt:egret.TouchEvent) => {
-            bird.x = evt.localX;
-            bird.y = evt.localY;
-        }, this)
+        
+        this.stage.addEventListener( egret.TouchEvent.TOUCH_BEGIN, ( evt:egret.TouchEvent )=>{
+            bird.x = evt.localX ;
+            bird.y = evt.localY ;
+        }, this );
     }
 }
+
